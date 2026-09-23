8eac3d8615c66d76be891c14dc477aac463ceb0c
Allen3857 <140083766+Allen3857@users.noreply.github.com>
2026-09-23 22:00:08 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// 把 Google Apps Script 部署後的 Web App URL 貼在下面。
-// 例如：https://script.google.com/macros/s/AKfycb.../exec
-// 尚未設定時留空即可；遊戲本身仍可正常執行，只是不會寫入 Google Sheet。
-const BACKEND_URL = "";
+// Google Apps Script Web App URL
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbxjwuqO78smAooFye19Ip4s9qV0PA7G5tZIXLFaRY8B7xD0PrMKF0xPU423zeV406oA/exec";
